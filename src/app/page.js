d04de1d0f27a34278b8f601158dd091640a909a8
RajaFaizan125 <faizan.akhtar@sprintx.net>
2023-12-20 18:12:47 +0500

    initail commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,305 @@
-import Image from 'next/image'
+"use client";
+import axios from "axios";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [inputVal, setInputVal] = useState("");
+  const changeHandler = (e) => {
+    setInputVal(e?.target?.value);
+  };
+
+  const options = {
+    method: "POST",
+    url: "http://ec2-34-206-43-97.compute-1.amazonaws.com:3000/tiktok",
+    headers: { "Content-Type": "application/json" },
+    data: { url: "https://www.tiktok.com/@hamzaig1/video/7279491364922920200" },
+  };
+
+  const onSubmit = async () => {
+    // const res = await axios.post(
+    //   "http://ec2-34-206-43-97.compute-1.amazonaws.com:3000/tiktok",
+    //   {
+    //     url: inputVal,
+    //   },
+    //   {
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+    // );
+
+    const videoLink =
+      "https://v16m-default.akamaized.net/6e4a7a966c056ef9d093b2cbbae37d34/658338b6/video/tos/alisg/sde/tos-alisg-pv-0037c001/owXFIvWCyEqACAAI8AgfDAlhEgzAcVAZawtZrO/?a=0&ch=0&cr=0&dr=0&lr=all&cd=0%7C0%7C0%7C1&cv=1&br=7448&bt=3724&bti=OUBzOTg7QGo6OjZAL3AjLTAzYCMxNDNg&ds=3&ft=iJOG.y7oZzv0PD1tmPtxg9wk.-MrBEeC~&mime_type=video_mp4&qs=13&rc=M3E0Njs6Zmd5bjMzODczNEBpM3E0Njs6Zmd5bjMzODczNEBfZmhicjRfMi9gLS1kMTFzYSNfZmhicjRfMi9gLS1kMTFzcw%3D%3D&l=202312201255354202C0642648F6198643&btag=e00048000&dpk=eSSr25sVvWSy28%2F4ZDh31%2FyuS9QFhWHXgsHPqXTL7gYuEdFHlntECO0bnUM5Y3EJX5mVYtQK1X37%2BVCiIcYEMljz993BI51Z8lZjpQ%3D%3D&dpm=aes-256-cfb&l=202312201255354202C0642648F6198643";
+    const videoBlob = new Blob([videoLink], { type: "video/mp4" });
+
+    const anchor = document.createElement("a");
+    anchor.href = window.URL.createObjectURL(videoBlob);
+    anchor.download = "video.mp4";
+
+    anchor.click();
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="main-container">
+      {/* <!--First Section Div--> */}
+      <div className="first-sec">
+        <nav className="navbar">
+          <div className="logo"> </div>
+          <ul className="list-item">
+            <li>Information</li>
+            <li>How To Use</li>
+          </ul>
+          <div className="btn-exntn">
+            <span>Chrome Extention</span>
+          </div>
+        </nav>
+        <div className="first-sec-hed">
+          <h3>TikTok Video Downloader</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Orci tincidunt justo
+            tincidunt ante elit. Quam arcu phasellus amet id amet duis. Iaculis
+            ele
+          </p>
+          <div className="search-dwnld">
+            <input
+              type="text"
+              placeholder="past video URL from TikTok"
+              onChange={changeHandler}
             />
-          </a>
+            <button onClick={onSubmit}>
+              Download
+              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAMNJREFUSEvtlbENwjAQRV8KVqBEoqJKT8UKbAEzMAIjkEEyQCpqsgC0rEAB+pIjBSsmZ6wIoeQaK/L5v/vns5IxcGQD6zM+wNO11OzcnOiEJ0Dv0I68RSH7J2AXaF4B7L29N532mIYAM6AC1p7QGdgAj1SAzs+B2q36vgO5W31z0Q4aATmQE4Uql4Ou+BogseYu1PtQJAF6HwrwO8ANWFhKNORcgaXy2mO6BY7AyiDwKeUCHIDSByTqdh+P/R9EF/H/gBdz+CUZeOsyDwAAAABJRU5ErkJggg==" />
+            </button>
+          </div>
+        </div>
+        <div className="angle-tik"> </div>
+      </div>
+      {/* <!-- Second Section Cards Div --> */}
+      <div className="secd-sec">
+        <div className="cards">
+          <div className="card">
+            <img src="assets/card_1_img.png" alt="" />
+            <p>Find Videos</p>
+            <span>
+              Copy the Tiktok video URL by clicking Share and choosing copy link
+            </span>
+          </div>
+          <div className="card">
+            <img src="assets/card_2_img.png" alt="" />
+            <p>Past Video</p>
+            <span>
+              Paste the TikTok video URL in the box above and hit the download
+              button.
+            </span>
+          </div>
+          <div className="card">
+            <img src="assets/card_3_img.png" alt="" />
+            <p>Download Videos</p>
+            <span>
+              Clicking Download Without watermark to download video mp4 without
+              watermark.
+            </span>
+          </div>
+        </div>
+      </div>
+      {/* <!-- Third Section --> */}
+      <div className="third-sec">
+        <p>TikTok Video Downloader</p>
+        <span>
+          Lorem ipsum dolor sit amet consectetur. Porttitor tempus eget id
+          curabitur massa. Blandit gravida imperdiet turpis nibh vivamus. Arcu
+          sed ac eget sit dolor parturient eget vitae. A porttitor natoque proin
+          aliquet ullamcorper. Eget at cras lorem lacus commodo ut. Nascetur
+          elementum ridiculus sit morbi rhoncus mauris cras ut. Porta gravida
+          tincidunt fusce dictum quam massa sem fusce risus. Condimentum quis
+          nisl quisque risus egestas. Felis posuere natoque hac massa. Sem urna
+          ut in rhoncus faucibus.
+        </span>
+        <div className="vactor">
+          <img
+            className="vactor-firstImg"
+            src="assets/third-sec-img1.svg"
+            alt=""
+          />
+          <img
+            className="vactor-firstImg2"
+            src="assets/third-sec-img2.svg"
+            alt=""
+          />
+          <img
+            className="vactor-firstImg3"
+            src="assets/third=sec-img3.svg"
+            alt=""
+          />
+          <img
+            className="vactor-firstImg4"
+            src="assets/third-sec-img4.svg"
+            alt=""
+          />
+          <div className="sit"></div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* <!--4th Section Fetures  --> */}
+      <div className="forth-sec">
+        <div className="img-sec">
+          <img className="shuilt-img" src="assets/shuilt-img.svg" alt="" />
+          <img
+            class="shuilt-face-img"
+            src="assets/shuilt-face-img.svg"
+            alt=""
+          />
+          <img class="hand" src="assets/hand-img.svg" alt="" />
+        </div>
+        <div class="content-featu">
+          <p class="head"> Features</p>
+          <div class="peragrap">
+            <li>
+              {" "}
+              <div class="circle"></div>
+              TikTok Downloader to download video TikTok{" "}
+              <span>No Watermark.</span>
+            </li>
+            <li>
+              <div class="circle"></div>
+              Download video TikTok using <span>your browser only.</span> No
+              other tools needed.
+            </li>
+            <li>
+              <div class="circle"></div>Download video TikTok
+              <span>No Watermark </span>with<span> Full HD quality.</span>
+            </li>
+            <li>
+              <div class="circle"></div>
+              Support Tik Tok Downloader,<span> Musically Downloader, </span>and
+              <span> Douyin (Chinese TikTok)</span>Downloader.
+            </li>
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      {/* <!-- Faq Section --> */}
+      <div class="five-sec" style={{ margin: "0px 30px" }}>
+        <div class="head">
+          <h2> Frequently Ask Question</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Porttitor tempus eget id
+            curabitur massa. Blandit gravida imperdiet turpis nibh vivamus.
           </p>
-        </a>
+        </div>
+        <div class="frame">
+          <div class="frst-q">
+            <h2>
+              Why does this tiktok Download say that I can,t download this
+              video?
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Porttitor tempus eget id
+              curabitur massa. Blandit gravida imperdiet turpis nibh vivamus.
+              Arcu sed ac eget sit dolor parturient eget vitae. A porttitor
+              natoque proin aliquet ullamcorper. Eget at cras lorem lacus
+              commodo ut. Nascetur elementum ridiculus sit morbi rhoncus mauris
+              cras ut. Porta gravida tincidunt fusce dictum quam massa sem fusce
+              risus. Condimentum quis nisl quisque risus egestas. Felis posuere
+              natoque hac massa. Sem urna ut in rhoncus faucibus.
+            </p>
+          </div>
+          <div class="cros-box">
+            <div class="line"></div>
+            <div class="line2"></div>
+          </div>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+        <div class="cards-feq">
+          <div class="card">
+            <p class="faqQ">How to get your Tiktok downlaod Link?</p>
+            <div class="card-lines">
+              <div class="line"> </div>
+              <div class="line2"> </div>
+            </div>
+          </div>
+          <p class="faqAns">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consectetur at fuga exercitationem ipsam inventore. Obcaecati, quas?
+            Fugit ut dolore non numquam natus quam, itaque in. Ipsum debitis
+            aliquam quas. Asperiores.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+          <div class="card">
+            <p class="faqQ">How to get your Tiktok downlaod Link?</p>
+            <div class="card-lines">
+              <div class="line"> </div>
+              <div class="line2"> </div>
+            </div>
+          </div>
+          <p class="faqAns">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consectetur at fuga exercitationem ipsam inventore. Obcaecati, quas?
+            Fugit ut dolore non numquam natus quam, itaque in. Ipsum debitis
+            aliquam quas. Asperiores.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <div class="card">
+            <p class="faqQ">How to get your Tiktok downlaod Link?</p>
+            <div class="card-lines">
+              <div class="line"> </div>
+              <div class="line2"> </div>
+            </div>
+          </div>
+          <p class="faqAns">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consectetur at fuga exercitationem ipsam inventore. Obcaecati, quas?
+            Fugit ut dolore non numquam natus quam, itaque in. Ipsum debitis
+            aliquam quas. Asperiores.
           </p>
-        </a>
+          <div class="card">
+            <p class="faqQ">How to get your Tiktok downlaod Link?</p>
+            <div class="card-lines">
+              <div class="line"> </div>
+              <div class="line2"> </div>
+            </div>
+          </div>
+          <p class="faqAns">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consectetur at fuga exercitationem ipsam inventore. Obcaecati, quas?
+            Fugit ut dolore non numquam natus quam, itaque in. Ipsum debitis
+            aliquam quas. Asperiores.
+          </p>
+        </div>
       </div>
-    </main>
-  )
+
+      <footer>
+        <div class="footer1">
+          <div class="fot-content">
+            <p class="foot-logo">Logo Here</p>
+            <span>
+              Lorem ipsum dolor sit amet consectetur. Porttitor tempus eget id
+              curabitur massa. Blandit gravida imperdiet.
+            </span>
+          </div>
+
+          <div class="links">
+            <ul>
+              <p>Quick Links</p>
+              <li>How To Use</li>
+              <li>Information</li>
+              <li>Terms of use</li>
+              <li>Privacy Policy</li>
+              <li>Contact Us</li>
+            </ul>
+            <ul>
+              <p>Other Links</p>
+              <li>Twitter Video Download</li>
+              <li>Pinterest Video Downloader</li>
+              <li>Insta Video Downloader</li>
+              <li>Youtube Video Downloader</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="line"> </div>
+        <div class="footer2">
+          <p>© LogoName 2023. All rights reserved.</p>
+        </div>
+      </footer>
+    </div>
+  );
 }
